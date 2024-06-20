6e92fddccde8ea0e25c75afcdca90a158fcb081f
Li Lin <guru.lin@gmail.com>
2024-06-20 11:30:59 +0800

    spring initializer generated code

---
+++ src/main/java/org/vino9/vinobank/coresim/CoreSimJApplication.java
@@ -0,0 +1,13 @@
+package org.vino9.vinobank.coresim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSimJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSimJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vino9/vinobank/coresim/CoreSimJApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vino9.vinobank.coresim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSimJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
